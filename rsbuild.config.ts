53b5d25a3bd78fef7218d7abcfd0bb556cc71a16
buyfakett <buyfakett@vip.qq.com>
2025-12-04 22:28:51 +0800

    同步更新

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,51 +3,50 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
 import path from 'path';
 
-let ENV_url: string;
+let ENV_url;
 
 try {
-  const {ENV_url: importedUrl} = require('./url.config');
-  ENV_url = importedUrl;
+    const {ENV_url: importedUrl} = require('./url.config');
+    ENV_url = importedUrl;
 } catch (error) {
-  // 如果导入文件失败，将 ENV_url 设置为空字符串
-  console.error('没有url.config.js文件:', error);
-  ENV_url = '';
+    console.error('没有url.config.js文件:', error);
+    ENV_url = '';
 }
 
 export default defineConfig({
-  source: {
-    entry: {
-      index: './src/main.tsx'
+    source: {
+        entry: {
+            index: './src/main.tsx'
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './')
-    }
-  },
-  plugins: [pluginReact(), pluginSass()],
-  server: {
-    proxy: {
-      '/api': {
-        target: ENV_url,
-        changeOrigin: true,
-      }
-    }
-  },
-  output: {
-    distPath: {
-      root: 'dist',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './')
+        }
     },
-    inlineScripts({ size }) {
-      return size < 10 * 1000;
+    plugins: [pluginReact(), pluginSass()],
+    server: {
+        proxy: {
+            '/api': {
+                target: ENV_url,
+                changeOrigin: true,
+            }
+        }
     },
-  },
-  performance: {
-    chunkSplit: {
-      // 代码拆分
-      strategy: 'split-by-size',
-      minSize: 30000, // 30k
-      maxSize: 500000, // 50k
+    output: {
+        distPath: {
+            root: 'dist',
+        },
+        inlineScripts({size}) {
+            return size < 10 * 1000;
+        },
     },
-  },
+    performance: {
+        chunkSplit: {
+            // 代码拆分
+            strategy: 'split-by-size',
+            minSize: 30000, // 30k
+            maxSize: 500000, // 50k
+        },
+    },
 });
